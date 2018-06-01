163375c61bbfb20fc46101974a9018d68d88c97e
Chloe Ng <chloegng@gmail.com>
2018-06-01 00:20:36 -0700

    create nav bar and create route links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCLBYDYEmXx0-IcxgpYMf8nR5zfvDG1loo",
+    authDomain: "clientpanelprod-a6ddc.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-a6ddc.firebaseio.com",
+    projectId: "clientpanelprod-a6ddc",
+    storageBucket: "clientpanelprod-a6ddc.appspot.com",
+    messagingSenderId: "335561310922"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCLBYDYEmXx0-IcxgpYMf8nR5zfvDG1loo",
+    authDomain: "clientpanelprod-a6ddc.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-a6ddc.firebaseio.com",
+    projectId: "clientpanelprod-a6ddc",
+    storageBucket: "clientpanelprod-a6ddc.appspot.com",
+    messagingSenderId: "335561310922"
+  }
 };
 
 /*
