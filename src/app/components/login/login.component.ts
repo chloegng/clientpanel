94fbc22aa910c28e203c7a47dfb68a73674541b6
Chloe Ng <chloegng@gmail.com>
2018-06-02 18:45:09 -0700

    login authentication

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService} from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private router: Router,
+    private flashMessages: FlashMessagesService
+) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if(auth) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+  onSubmit() {
+    this.authService.login(this.email, this.password)
+      // because you are doing a promise, you must add a .then
+    .then(res => {
+      this.flashMessages.show('You are now logged in', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    })
+    .catch(err => {
+      this.flashMessages.show(err.message, {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    });
   }
 
 }
