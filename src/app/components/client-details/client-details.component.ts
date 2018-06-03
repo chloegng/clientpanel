ac7c7b205f6744b314c482794d04b95c08beea71
Chloe Ng <chloegng@gmail.com>
2018-06-02 17:40:07 -0700

    complete crud application, allows edit and delete client

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from '../../services/client.service';
 import { Client } from '../../models/Client';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { FlashMessagesModule } from 'angular2-flash-messages/module/module';
 
 @Component({
   selector: 'app-client-details',
@@ -19,7 +20,7 @@ export class ClientDetailsComponent implements OnInit {
     private clientService: ClientService,
     private router: Router,
     private route: ActivatedRoute,
-    private flashMessages: FlashMessagesService
+    private flashMessages: FlashMessagesService,
   ) { }
 
   ngOnInit() {
@@ -42,5 +43,14 @@ export class ClientDetailsComponent implements OnInit {
     this.flashMessages.show('Balance updated', {
       cssClass: 'alert-success', timeout: 4000
     });
+  }
+  onDeleteClick() {
+    if(confirm('Are you sure?')) {
+      this.clientService.deleteClient(this.client);
+      this.flashMessages.show('Client removed', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    }
   }
 }
