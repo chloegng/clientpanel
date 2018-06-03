ed6706d669eef2b54ac0de108576352fd0a164c8
Chloe Ng <chloegng@gmail.com>
2018-06-02 23:27:32 -0700

    add settings page, settings will change local storage

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -9,9 +9,16 @@ export class SettingsService {
     disableBalanceOnAdd: true,
     disableBalanceOnEdit: true,
   }
-  constructor() { }
+  constructor() {
+    if(localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings);
   }
 }
